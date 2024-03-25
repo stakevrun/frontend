3a9d638fed91149772dcd23d5e21418b1fcb105f
frankmchugh <frank.b.mchugh@gmail.com>
2024-03-25 10:14:48 +0000

    Fixing dependency bugs

--- pages/components/accountMain.tsx
+++ pages/components/accountMain.tsx
@@ -1888,14 +1888,14 @@ const AccountMain: NextPage = () => {
                       <span className="block text-gray-500">Finished homeworks</span>
                     </div>
                   </div>
-                  <div className="flex items-center justify-center p-8 bg-white shadow rounded-lg mb-5">
+                  <div className="flex items-center  p-8 bg-white shadow rounded-lg mb-5">
 
                   <div className="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-blue-600 bg-blue-100 rounded-full mr-6">
                       <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
                       </svg>
                     </div>
-                    <div className="w-full max-w-3xl flex  items-center justify-center gap-7 text-center">
+                    <div className=" max-w-3xl flex flex-col items-start justify-start gap-2 text-left">
                    
 
 
