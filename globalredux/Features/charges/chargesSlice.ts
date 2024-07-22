25f0bf13c0ef46e12ede4ad14c074a86f0abbe65
Bethany Pietroniro <bethany.pietroniro28@gmail.com>
2024-07-21 21:55:41 -0400

    bulk reformatting so that subsequent PR diffs are clearer

--- globalredux/Features/charges/chargesSlice.ts
+++ globalredux/Features/charges/chargesSlice.ts
@@ -1,65 +1,38 @@
-'use client'
+"use client";
 
-
-import { createSlice } from '@reduxjs/toolkit'
-
-
+import { createSlice } from "@reduxjs/toolkit";
 
 type edittedObject = {
-    date: string
-    fee: number
-    pubkey: string
-}
-
-
-
+  date: string;
+  fee: number;
+  pubkey: string;
+};
 
 type StateType = {
-    data: number
-    entryData: Array<edittedObject>
-   
-}
+  data: number;
+  entryData: Array<edittedObject>;
+};
 
 const initialState: StateType = {
-    data: 0,
-    entryData: [{date: "defaultState", fee:0, pubkey: "defaultState"}]
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  data: 0,
+  entryData: [{ date: "defaultState", fee: 0, pubkey: "defaultState" }],
+};
 
 export const chargesDataSlice = createSlice({
-    name: 'chargesData',
-    initialState,
-    reducers: {
-        getChargesData: (state, action) => {
-            // Mutate the state to update it
-            state.data = action.payload;
-          
-        },
-        getChargesEntryData: (state, action) => {
-            // Mutate the state to update it
-            state.entryData = action.payload;
-          
-        },
-      
-    }
-})
+  name: "chargesData",
+  initialState,
+  reducers: {
+    getChargesData: (state, action) => {
+      // Mutate the state to update it
+      state.data = action.payload;
+    },
+    getChargesEntryData: (state, action) => {
+      // Mutate the state to update it
+      state.entryData = action.payload;
+    },
+  },
+});
 
-export const { getChargesData, getChargesEntryData} = chargesDataSlice.actions;
+export const { getChargesData, getChargesEntryData } = chargesDataSlice.actions;
 
 export default chargesDataSlice.reducer;
