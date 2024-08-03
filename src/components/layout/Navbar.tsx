df308fd361b185e9a458ac894eb3cde835bccb97
Ramana Kumar <ramana@member.fsf.org>
2024-08-03 12:16:01 +0100

    Put link to account and home in nav bar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -6,7 +6,10 @@ export function Navbar() {
 
   return (
     <header className="p-2 flex flex-row justify-around items-center">
+      <a className="ring rounded hover:bg-sky-200" href="/">Home</a>
       <ConnectButton />
+      {isConnected && <a className="ring rounded hover:bg-sky-200" href="/account">Account</a>}
+      {/*all these are probably unnecessary: they are in ConnectButton already*/}
       <div>Address: {address}</div>
       <div>Chain ID: {chainId}</div>
       <div>Connected: {isConnected.toString()}</div>
