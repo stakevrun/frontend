4972e9f0ea10a3e78f8830e722346e9bbd0397f2
Ramana Kumar <ramana@member.fsf.org>
2024-08-04 14:00:15 +0100

    Start creating register with Rocket Pool form

--- src/components/layout/IfRegistered.tsx
+++ src/components/layout/IfRegistered.tsx
@@ -1,11 +1,36 @@
 import { useReadContract } from "wagmi";
 import { abi } from "../../abi/rocketNodeManagerABI";
 import { useRocketAddress } from "../../hooks/useRocketAddress";
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useState } from "react";
 
 export const RegistrationForm: FC<{isRegistered: boolean}> = ({isRegistered}) => {
+  const [selectedTimezone, setSelectedTimezone] = useState("");
+  const handleSelectTimezone = (e) => {
+    setSelectedTimezone(e.target.value || "");
+  };
   return (
-    <p>placeholder: form to register with rocket pool, since isRegistered={isRegistered?.toString()} : {typeof isRegistered}</p>
+    <>
+    <p>Debug Info: isRegistered={isRegistered?.toString()} : {typeof isRegistered}</p>
+    <label>
+    <span className="pr-1">Node Timezone:</span>
+    <input
+      type="text"
+      list="timezones"
+      placeholder="Region/City"
+      onChange={handleSelectTimezone}
+      value={selectedTimezone}
+    />
+    </label>
+    <datalist id="timezones">
+     {Intl.supportedValuesOf('timeZone').map(timezone => (
+       <option>{timezone}</option>
+     ))}
+    </datalist>
+    <button
+     className="border rounded"
+    >Register
+    </button>
+    </>
   );
 };
 
@@ -17,7 +42,7 @@ export const IfRegistered: FC<{children: ReactNode, address: `0x${string}` | und
   return (
     isPending ? <p>Fetching node registration status...</p> :
       error ? <p>Error reading getNodeExists: {error.message}</p> :
-        isRegistered ? children :
+        isRegistered ? <><p>Debug Info: Registered with Rocket Pool.</p> {children}</> :
           <RegistrationForm isRegistered={isRegistered} />
   );
 };
