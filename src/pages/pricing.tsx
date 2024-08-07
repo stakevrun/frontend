179ffd1853abe7d43582d4c725ab263cd12070cd
Bethany Pietroniro <bethany.pietroniro28@gmail.com>
2024-08-06 23:32:59 -0400

    Initial attempt at fetching prices

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,9 +1,38 @@
 import type { NextPage } from "next";
+import { useState, useEffect } from "react";
+import { useChainId } from "wagmi";
 
+// work in progress
 const Pricing: NextPage = () => {
+  const chainId = useChainId();
+  const [validUntil, setValidUntil] = useState<number | "now" | undefined>();
+  const [pricesPerDay, setPricesPerDay] = useState({}); // TODO: create type
+
+  useEffect(() => {
+    // TODO: error handling
+    const getPricing = async () => {
+      const { validUntil, pricesPerDay } = await fetch(
+        `https://fee.vrün.com/${chainId}/prices`
+      ).then((r) => {
+        return r.json();
+      });
+
+      setValidUntil(validUntil);
+      setPricesPerDay(pricesPerDay);
+    };
+
+    getPricing();
+  }, [chainId]);
   return (
-    <h1>Pricing</h1>
-  )
+    <div>
+      <h1>Pricing</h1>
+      {Object.keys(pricesPerDay).map((tokenChainId, idx) => {
+        return (
+          <p key={idx}>{tokenChainId}</p>
+        )
+      })}
+    </div>
+  );
 };
 
 export default Pricing;
