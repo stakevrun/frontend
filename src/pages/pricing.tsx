53442b801df7befded317fed43460f4fd4c3514c
Ramana Kumar <ramana@member.fsf.org>
2024-08-07 15:11:03 +0100

    Refactor and fix type errors

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -6,56 +6,77 @@ import { type ContractFunctionParameters, formatUnits } from "viem";
 // work in progress
 const Pricing: NextPage = () => {
   const chainId = useChainId();
-  const [validUntil, setValidUntil] = useState<number | "now" | undefined>();
-  const [pricesPerDay, setPricesPerDay] = useState({}); // TODO: create type
+
+  type pricesPerDayByTokenType = {[tokenAddress: `0x${string}`]: bigint};
+  type pricesPerDayType = {[chainId: string]: pricesPerDayByTokenType};
+  interface pricingRowType {tokenAddress: `0x${string}`, tokenChainId: number, price: bigint};
+  interface pricesStateType {
+    validUntil: number | "now",
+    pricesPerDay: pricesPerDayType,
+    pricingRowData: pricingRowType[],
+    contractReads: ContractFunctionParameters[]
+  }
+
+  const [prices, setPrices] = useState<pricesStateType | undefined>();
 
   useEffect(() => {
     // TODO: error handling
     const getPricing = async () => {
-      const { validUntil, pricesPerDay } = await fetch(
+      const { validUntil, pricesPerDay }: { validUntil: number | "now", pricesPerDay: pricesPerDayType } = await fetch(
         `https://fee.vrün.com/${chainId}/prices`
       ).then((r) => {
         return r.json();
       });
 
-      setValidUntil(validUntil);
-      setPricesPerDay(pricesPerDay);
+      const pricingRowData =
+        Object.entries(pricesPerDay).flatMap(
+          ([tokenChainId, chainPrices]: [string, pricesPerDayByTokenType]) =>
+          Object.entries(chainPrices).map(
+            ([tokenAddress, price]: [string, bigint]) => (
+              {
+                tokenChainId: Number(tokenChainId),
+                tokenAddress: tokenAddress as `0x${string}`,
+                price,
+              }
+            )
+          )
+      );
+
+      const contractReads =
+        pricingRowData.flatMap(
+          ({tokenChainId, tokenAddress}) => {
+            interface readTokenInput {functionName: string, outputType: string};
+            const readToken = ({functionName, outputType}: readTokenInput) => ({
+              chainId: tokenChainId,
+              address: tokenAddress as `0x${string}`,
+              abi: [{name: functionName, type: "function" as const, stateMutability: "view" as const, inputs: [], outputs: [{name: functionName, type: outputType}]}],
+              functionName
+            });
+            return [
+              readToken({functionName: "decimals", outputType: "uint8"}),
+              readToken({functionName: "name", outputType: "string"}),
+            ];
+          }
+      );
+
+      setPrices({
+        validUntil,
+        pricesPerDay,
+        pricingRowData,
+        contractReads,
+      })
     };
 
     getPricing();
   }, [chainId]);
 
-  interface pricingRowType {tokenAddress: string, tokenChainId: string, price: bigint};
-  const pricingRowData: pricingRowType[] = Object.entries(pricesPerDay).flatMap(
-    ([tokenChainId, chainPrices]) =>
-    Object.entries(chainPrices).map( // TODO: type error here would be fixed by adding interface for pricesPerDay
-      ([tokenAddress, price]) => ({tokenChainId, tokenAddress, price: price as bigint})
-    )
-  );
-
-  const allReads: ContractFunctionParameters[] = pricingRowData.flatMap(
-    ({tokenChainId, tokenAddress}) => {
-      interface readTokenInput {functionName: string, outputType: string};
-      const readToken = ({functionName, outputType}: readTokenInput) => ({
-        chainId: tokenChainId,
-        address: tokenAddress as `0x${string}`,
-        abi: [{name: functionName, type: "function" as const, stateMutability: "view" as const, inputs: [], outputs: [{name: functionName, type: outputType}]}],
-        functionName
-      });
-      return [
-        readToken({functionName: "decimals", outputType: "uint8"}),
-        readToken({functionName: "name", outputType: "string"}),
-      ];
-    }
-  );
-
-  const {data: results, error, status} = useReadContracts({contracts: allReads});
+  const {data: results, error, status} = useReadContracts({contracts: prices?.contractReads});
 
   return (
     <div>
       <h1>Pricing</h1>
       {error ? <p>Error reading token contracts: {error.message}</p> :
-       results ? (
+       prices && results ? (
         <table>
           <thead>
             <tr>
@@ -65,11 +86,11 @@ const Pricing: NextPage = () => {
             </tr>
           </thead>
           <tbody>
-          {pricingRowData.map(({tokenChainId, tokenAddress, price}, i) => {
+          {prices.pricingRowData.map(({tokenChainId, tokenAddress, price}, i) => {
             const [tokenDecimals, tokenName] = results.slice(i * 2, 2);
             return (
               <tr>
-                <td>{tokenChainId}</td>
+                <td>{tokenChainId.toString()}</td>
                 <td>{tokenName.result as string}({tokenAddress})</td>
                 <td>{formatUnits(price, tokenDecimals.result as number)}</td>
               </tr>
