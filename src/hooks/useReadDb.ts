8c734b1b6e8edf220e806278cb88e771dbdb2a60
Ramana Kumar <ramana@member.fsf.org>
2024-08-08 09:51:11 +0100

    Start drafting hook to read the db
    Using TanStack query to handle caching, refetching, etc. We should use
    this for the admins read too (although it would not use this hook since
    this one assumes we're using the chainId/address routes).

---
+++ src/hooks/useReadDb.ts
@@ -0,0 +1,23 @@
+import { useChainId, useAccount } from "wagmi";
+import { useQuery } from "@tanstack/react-query";
+
+export function useReadDb(
+  { path, searchParams } : { path: string, searchParams?: Record<string, string> }
+) {
+  const chainId = useChainId();
+  const {address} = useAccount();
+  const qs = searchParams && (new URLSearchParams(searchParams)).toString();
+  const queryString = qs ? `?${qs}` : '';
+  const queryKey = [chainId, address, path, queryString];
+  const url = `https://api.vrün.com/${chainId}/${address}/${path}${queryString}`;
+  const queryFn = () =>
+    fetch(url)
+      .then(
+        r => r.status === 200 ?
+          r.json() :
+          r.text().then(msg => { throw new Error(`${r.status} error fetching ${url}: ${msg}`) }));
+  return useQuery({
+    queryKey,
+    queryFn,
+  });
+};
