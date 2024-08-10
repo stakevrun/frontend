01346b556550165c87dbb66c00efb5702fa67cc9
Bethany Pietroniro <bethany.pietroniro28@gmail.com>
2024-08-10 00:05:35 -0400

    Footer styling

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="p-4 flex flex-col lg:flex-row items-center justify-between">
-      <p>Vrün website and staking service Copyright 2024</p>
-      <div className="flex flex-col lg:flex-row gap-8">
-        <Link href="/pricing">Pricing</Link>
-        <Link href="/faq">FAQ</Link>
-        <Link href="/terms">Terms</Link>
+    <footer className="mt-14 mb-8 p-10 flex flex-col justify-between border-t">
+      <div className="text-md w-full p-6 flex flex-row justify-center gap-16">
+        <Link href="/pricing" className="text-zinc-500 hover:text-zinc-800">Pricing</Link>
+        <Link href="/faq" className="text-zinc-500 hover:text-zinc-800">FAQ</Link>
+        <Link href="/terms" className="text-zinc-500 hover:text-zinc-800">Terms</Link>
       </div>
+      <p className="p-6 text-xs text-center">Vrün website and staking service © 2024</p>
     </footer>
   );
 }
--- src/components/markdownToDisclosure.tsx
+++ src/components/markdownToDisclosure.tsx
@@ -3,8 +3,8 @@ import {
   Disclosure,
   DisclosureButton,
   DisclosurePanel,
-  Transition,
 } from "@headlessui/react";
+import { CiCirclePlus, CiCircleMinus } from "react-icons/ci";
 
 interface HeaderContentPair {
   header: ReactNode;
@@ -17,7 +17,7 @@ const MarkdownToDisclosure: React.FC<{ children: ReactNode }> = ({
   // Split the children into an array of elements
   const elements = React.Children.toArray(children);
 
-  // Group headers with their following paragraphs
+  // Group headers with their corresponding content
   const groups: HeaderContentPair[] = [];
   for (let i = 0; i < elements.length; i += 2) {
     if (i + 1 < elements.length) {
@@ -32,14 +32,21 @@ const MarkdownToDisclosure: React.FC<{ children: ReactNode }> = ({
     <div className="w-full p-2 mx-auto rounded-2xl">
       {groups.map(({ header, content }, index) => (
         <Disclosure key={index} as="div" className="mt-2">
-          <DisclosureButton className="w-full px-4 py-2 text-md font-medium text-center text-zinc-900 bg-zinc-100 rounded-lg hover:bg-zinc-200 focus:outline-none focus-visible:ring-1 focus-visible:ring-zinc-400 focus-visible:ring-opacity-75">
-            <span>
-              {React.isValidElement(header) ? header.props.children : header}
-            </span>
-          </DisclosureButton>
-          <DisclosurePanel className="px-4 pt-1 pb-4 text-sm text-gray-500">
-            {content}
-          </DisclosurePanel>
+          {({ open }) => (
+            <>
+              <DisclosureButton className="w-full px-4 py-2 text-md font-medium text-zinc-900 bg-zinc-100 rounded-lg hover:bg-zinc-200 focus:outline-none focus-visible:ring-1 focus-visible:ring-zinc-400 focus-visible:ring-opacity-75">
+                <span>
+                  {React.isValidElement(header)
+                    ? header.props.children
+                    : header}
+                {/* {open ? <CiCircleMinus /> : <CiCirclePlus />} */}
+                </span>
+              </DisclosureButton>
+              <DisclosurePanel className="px-4 pt-1 pb-4 text-sm text-gray-500">
+                {content}
+              </DisclosurePanel>
+            </>
+          )}
         </Disclosure>
       ))}
     </div>
