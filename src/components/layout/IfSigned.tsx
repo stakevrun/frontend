b07da05c027e6618b897ed9c8dfed85372da796f
Ramana Kumar <ramana@member.fsf.org>
2024-08-10 16:06:22 +0100

    Start building the sign terms form

--- src/components/layout/IfSigned.tsx
+++ src/components/layout/IfSigned.tsx
@@ -1,5 +1,24 @@
 import { FC, ReactNode } from "react";
+import { useSignTypedData } from "wagmi";
 import { useReadDb } from "../../hooks/useReadDb";
+import { types, declaration, useApiDomain } from "../../hooks/useApiTypes";
+
+export const SignTermsForm: FC<{}> = ({}) => {
+  const domain = useApiDomain();
+  const {signTypedData, data, error} = useSignTypedData();
+  const primaryType = "AcceptTermsOfService";
+  const message = { declaration };
+  const handler = () => {
+    signTypedData({types, domain, primaryType, message});
+  };
+
+  return (
+    <button
+      className="bg-blue-500 self-center xl:self-start hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md"
+      onClick={handler}>
+    Sign Terms
+    </button>);
+};
 
 export const IfSigned: FC<{
   children: ReactNode,
@@ -8,5 +27,5 @@ export const IfSigned: FC<{
   const isSigned = data?.status === 200;
   if (error) return (<p>Error checking ToS signature: {error.message}</p>);
   else if (isSigned) return (<><p>Debug Info: Signed ToS</p>{children}</>);
-  else return (<p>Placeholder: form to sign ToS</p>);
+  else return <SignTermsForm />;
 };
