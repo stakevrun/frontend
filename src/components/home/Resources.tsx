7f17dd8af38334fd72f436bc471bdf3001dd7155
Bethany Pietroniro <bethany.pietroniro28@gmail.com>
2024-08-11 22:14:37 -0400

    Removed Home.module.css and transitioned to Card component (styles need some work but it's a start)

--- src/components/home/Resources.tsx
+++ src/components/home/Resources.tsx
@@ -2,8 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@headlessui/react";
 import { FaMoneyBillWave, FaGithub, FaEthereum } from "react-icons/fa";
-
-import styles from "../../styles/Home.module.css"; // remove this once styles are translated into Tailwind
+import { Card, CardIcon, CardContent } from "../layout/Card";
 
 const Resources = () => {
   return (
@@ -15,88 +14,70 @@ const Resources = () => {
       <div className="mx-auto h-auto w-[90%] lg:w-auto rounded-[30px] border-4 border-[#6C6C6C] bg-[#222222] p-3 lg:p-5 shadow-2xl lg:h-[40rem] lg:max-w-5xl">
         <div className="grid h-full w-full grid-cols-1 gap-4 overflow-hidden rounded-2xl bg-gray-100">
           <div className="flex flex-col items-center justify-center max-w-3xl w-full sm:flex-row sm:flex-wrap">
-            <Link href="/account" className="self-start mt-3">
-              <div className={styles.card}>
-                <div className="inline-flex flex-shrink-8 items-center justify-center h-16 w-16 text-blue-600 bg-blue-100 rounded-full mr-6">
+            <Link href="/account">
+              <Card>
+                <CardIcon>
                   <FaMoneyBillWave className="text-3xl text-green-500  w-[70px]" />
-                </div>
-                <div>
-                  <h2 className="font-bold text-black">
-                    Stake RPL for your Minipools &rarr;
-                  </h2>
-                  <p>We provide an onsite service for staking your RPL</p>
-
-                  <Button className="btn-primary text-xs">Stake RPL</Button>
-                </div>
-              </div>
+                </CardIcon>
+                <CardContent
+                  title="Stake RPL for your Minipools"
+                  description="We provide an onsite service for staking your RPL."
+                ></CardContent>
+                <Button className="btn-primary text-xs">Stake RPL</Button>
+              </Card>
             </Link>
 
-            <div className={styles.card}>
-              <div className="inline-flex flex-shrink-8 items-center justify-center h-16 w-16 text-blue-600 bg-blue-100 rounded-full mr-6">
-                <FaEthereum className="text-3xl text-blue-500  w-[70px]" />
-              </div>
+            <Link href="/faq">
+              <Card>
+                <CardIcon>
+                  <FaEthereum className="text-3xl text-blue-500  w-[70px]" />
+                </CardIcon>
+                <CardContent
+                  title="New to Validators & Staking?"
+                  description="Learn how Vrün interacts with Ethereum and the beacon chain."
+                ></CardContent>
+                <Button className="btn-primary text-xs">Get started!</Button>
+              </Card>
+            </Link>
 
-              <div>
-                <h2 className="text-black font-bold">
-                  New to Validators & Staking? &rarr;
-                </h2>
-                <p>
-                  Learn how Vrün interacts with Ethereum and the beacon chain.
-                </p>
-                <Link href="/faqs">
-                  <Button className="btn-primary text-xs">Get started!</Button>
-                </Link>
-              </div>
-            </div>
+            <a
+              href="https://docs.rocketpool.net/overview/contracts-integrations"
+              target="_blank"
+              className="lg:self-start mt-3"
+            >
+              <Card>
+                <CardIcon>
+                  <Image
+                    width={70}
+                    height={70}
+                    alt="Rocket Pool Logo"
+                    src={"/images/rocketPlogo.png"}
+                  />
+                </CardIcon>
+                <CardContent
+                  title="Rocket Pool Docs"
+                  description="Learn about the under the hood functions which help to power Vrün!"
+                ></CardContent>
+                <Button className="btn-primary text-xs">Go to Docs</Button>
+              </Card>
+            </a>
 
-            <div className={styles.card}>
-              <div className="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-blue-600 bg-blue-100 rounded-full mr-6">
-                <Image
-                  width={70}
-                  height={70}
-                  alt="Rocket Pool Logo"
-                  src={"/images/rocketPlogo.png"}
-                />
-              </div>
-              <div>
-                <h2 className="text-black font-bold">
-                  Rocket Pool Docs &rarr;
-                </h2>
-                <p>
-                  Learn about the under the hood functions which help to power
-                  Vrün!
-                </p>
-
-                <a
-                  href="https://docs.rocketpool.net/overview/contracts-integrations"
-                  target="_blank"
-                  className="self-start mt-3"
-                >
-                  <Button className="btn-primary text-xs">Go to Docs</Button>
-                </a>
-              </div>
-            </div>
-
-            <div className={styles.card}>
-              <div className="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-blue-600 bg-blue-100 rounded-full mr-6">
-                <FaGithub className="text-yellow-500 text-2xl" />
-              </div>
-              <div>
-                <h2 className="font-bold text-black text-lg mb-1">
-                  Vrün GitHub documentation &rarr;
-                </h2>
-                <p className="mb-3">
-                  Find in-depth information and routes for our API.
-                </p>
-                <a
-                  href="https://github.com/stakevrun/db"
-                  target="_blank"
-                  className="self-start mt-3"
-                >
-                  <Button className="btn-primary text-xs">Go to Github</Button>
-                </a>
-              </div>
-            </div>
+            <a
+              href="https://github.com/stakevrun/db"
+              target="_blank"
+              className="lg:self-start mt-3"
+            >
+              <Card>
+                <CardIcon>
+                  <FaGithub className="text-yellow-500 text-2xl" />
+                </CardIcon>
+                <CardContent
+                  title="Vrün GitHub documentation"
+                  description="Find in-depth information and routes for our API."
+                ></CardContent>
+                <Button className="btn-primary text-xs">Go to Github</Button>
+              </Card>
+            </a>
           </div>
         </div>
       </div>
--- src/components/layout/Card.tsx
+++ src/components/layout/Card.tsx
@@ -1,13 +1,43 @@
-export const CardTitle = () => {
+import { ReactNode, FC } from "react";
 
+type CardProps = {
+  children: ReactNode;
+  className?: string;
 };
 
-export const CardContent = () => {
-
+export const Card = ({ children, className = "" }: CardProps) => {
+  return (
+    <div
+      className={`group m-4 p-6 text-left text-inherit no-underline border border-gray-200 rounded-lg transition-colors duration-150 ease-in-out hover:border-blue-500 focus:border-blue-500 active:border-blue-500 max-w-[350px] ${className}`}
+    >
+      {children}
+    </div>
+  );
 };
 
-export const Card = ({ children, href, className = '' }) => (
-  <a href={href} className={`m-4 p-6 text-left text-inherit no-underline border border-gray-200 rounded-lg transition-colors duration-150 ease-in-out hover:text-blue-500 hover:border-blue-500 focus:text-blue-500 focus:border-blue-500 active:text-blue-500 active:border-blue-500 max-w-[350px] ${className}`}>
-    {children}
-  </a>
-);
+export const CardContent = ({
+  title,
+  description,
+  className = "",
+}: {
+  title: string;
+  description: string;
+  className?: string;
+}) => {
+  return (
+    <div>
+      <h2 className={`font-bold text-black ${className}`}>{title}</h2>
+      <p className="my-2 text-sm">{description}</p>
+    </div>
+  );
+};
+
+export const CardIcon = ({ children, className = "" }: CardProps) => {
+  return (
+    <div
+      className={`inline-flex flex-shrink-8 items-center justify-center h-16 w-16 text-blue-600 bg-blue-100 rounded-full mr-6 ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
