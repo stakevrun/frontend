a83f3ebaa171dd6a37d3187adebd50726682749f
Bethany Pietroniro <bethany.pietroniro28@gmail.com>
2024-08-15 16:56:42 -0400

    Update terms

--- src/components/account/Overview.tsx
+++ src/components/account/Overview.tsx
@@ -1,7 +1,17 @@
+import { FaCoins, FaEthereum } from "react-icons/fa";
+
 const Overview = () => {
   return (
-    <h1>Account overview goes here</h1>
-  )
+    <>
+      <h1>Account Overview</h1>
+      {/* <div className="panel">
+        <FaEthereum className="text-3xl text-blue-500  w-[70px]" />
+      </div>
+      <div className="panel">
+        <FaCoins />
+      </div> */}
+    </>
+  );
 };
 
 export default Overview;
