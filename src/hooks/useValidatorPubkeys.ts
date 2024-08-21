140b3ba02e45c2d08f069d4ba042a96a747a4e73
Ramana Kumar <ramana@member.fsf.org>
2024-08-21 12:33:50 +0100

    Start on a useValidatorPubkeys hook

---
+++ src/hooks/useValidatorPubkeys.ts
@@ -0,0 +1,26 @@
+import { useAccount } from "wagmi";
+import { useQueries, type QueryFunctionContext } from "@tanstack/react-query";
+import { useReadDb } from "./useReadDb";
+
+export function useValidatorPubkeys() {
+  const {address, chainId} = useAccount();
+  const {data: nextIndexData, error: nextIndexError} = useReadDb({path: 'nextindex'});
+  const ids : Array<number> = Array.from(Array(nextIndexData?.value || 0).keys());
+  const queryFn = ({queryKey} : QueryFunctionContext) => {
+    const i = queryKey.at(-1);
+    return fetch(`https://api.vrün.com/${chainId}/${address}/pubkey/${i}`).then(
+      r => r.status === 200 ? r.json() :
+           r.text().then(t => { throw new Error(`${r.status} error fetching pubkey ${i}: ${t}`); })
+    );
+  };
+  const results = useQueries({
+    queries: ids.map(i => ({
+      queryKey: ['vrün', chainId, address, 'pubkey', i],
+      queryFn,
+    }))
+  });
+  return {
+    data: results.map(r => r.data),
+    error: nextIndexError || results.find(r => r.error),
+  };
+};
