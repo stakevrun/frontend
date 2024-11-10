3570ad3dcc45db61c3dd94a5eac4b535d004ed85
Ramana Kumar <ramana@member.fsf.org>
2024-11-10 21:30:39 +0000

    Fix typo
    Looks like it might have been an auto word wrap error

--- src/components/layout/IfRegistered.tsx
+++ src/components/layout/IfRegistered.tsx
@@ -43,8 +43,8 @@ export const RegistrationForm: FC<{
           </label>
         </div>
         <div className="md:w-2/3">
-          <input 
-            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name"
+          <input
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 inline-full-name"
             id="timezone"
             type="text"
             list="timezones"
