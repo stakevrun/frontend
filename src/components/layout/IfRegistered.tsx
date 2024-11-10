a03c0f5a58b8b45dcdfaf20760c2669f4f02f39a
Ramana Kumar <ramana@member.fsf.org>
2024-11-10 21:33:16 +0000

    Better fix for the label for thing
    inline-full-name is not a className but actually maybe came from a
    template on using tailwind i guess

--- src/components/layout/IfRegistered.tsx
+++ src/components/layout/IfRegistered.tsx
@@ -38,13 +38,13 @@ export const RegistrationForm: FC<{
       <p className="my-4 text-center">Placeholder: Rocket Pool logo, Register form heading</p>
       <div className="md:flex md:items-center mt-6">
         <div className="md:w-1/3">
-          <label for="timezone" className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+          <label htmlFor="timezone" className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
             Node Timezone:
           </label>
         </div>
         <div className="md:w-2/3">
           <input
-            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 inline-full-name"
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             id="timezone"
             type="text"
             list="timezones"
