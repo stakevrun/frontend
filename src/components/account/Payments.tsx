ecdf71679079a54d14deab719f1ca44daf17c43b
owlot <95369289+owlot@users.noreply.github.com>
2024-12-02 20:27:49 +0100

    feat: add payment functionality

--- src/components/account/Payments.tsx
+++ src/components/account/Payments.tsx
@@ -1,37 +1,101 @@
-import { Input, Button } from "@headlessui/react";
-import { FaCoins } from "react-icons/fa";
-import { useQuery } from "@tanstack/react-query";
+import { type FC, type ReactNode, useEffect } from "react";
 import { useState } from "react";
-
-/*
-const [credits, setCredits] = useState<number | undefined>(); //chainId/address/credits
-// for this, need the pubkeys associated with the address - maybe do the validator list page first to get that component?
-const [charges, setCharges] = useState<chargesType | undefined>(); //chainId/address/pubkey/charges
-*/
+import { useSignTypedData } from "wagmi";
+import { FaCoins } from "react-icons/fa";
+import type { UseQueryResult } from "@tanstack/react-query";
+import { Input, Button } from "@headlessui/react";
+import { PayForm } from "../layout/PayForm";
+import { SignPaymentForm } from "../layout/SignPaymentForm";
+import { useSignPaymentTransaction } from "../../hooks/useSignPaymentTransaction";
+import { useReadFee } from "../../hooks/useReadFee";
+import { useVrunPrices } from "../../hooks/useVrunPrices";
 
 const Payments = () => {
+  const [creditError, setCreditError] = useState();
+  const [payHash, setPayHash] = useState();
+  const [paySuccess, setPaySuccess] = useState();
+  const [currentBalance, setCurrentBalance] = useState();
+  const [hasError, setHasError] = useState(false);
+  const [error, setError] = useState("");
+
+  const {data: creditData, isLoading: creditIsLoading, refetch: creditRefetch} = useReadFee({path: 'credits'});
+  const {prices, isLoading: pricesIsLoading, error: pricesError} = useVrunPrices();
+  const {data: signData, error: signError} = useSignPaymentTransaction(payHash, prices);
+
+  useEffect(() => {
+    if(creditError || pricesError || signError) {
+      setHasError(true);
+      setError(`${pricesError ? pricesError + "\n" : ""}${creditError ? creditError + "\n" : ""}${signError ? signError + "\n" : ""}`);
+    }
+
+    if(!creditError && !pricesError && !signError) {
+      setHasError(false);
+    }
+  }, [creditError, pricesError, signError]);
+
+  useEffect(() => {
+    if (signError) {
+      setPayHash("");
+    } else {
+      if (signData) {
+        setPaySuccess(`Successfully added ${signData.numDays} days to your balance`);
+        setPayHash("");
+        creditRefetch();
+      }
+    }
+  }, [signError, signData]);
+
+  useEffect(() => {
+    if(creditData) {
+      if(creditData.status !== 200) {
+        if(creditData.status === 404) {
+          setCreditError("You have not yet purchased any credits.");
+        } else {
+          setCreditError("Something went wrong requesting your credit balance.");
+        }
+      }
+      setCurrentBalance(creditData.value || 0)
+    }
+  }, [creditData]);
+
   return (
-    <>
+    <section className="grid gap-4 my-10 py-5">
+      {!signError && paySuccess && (
+      <div className="self-center whitespace-pre-wrap break-words mt-4 border border-green-500 rounded p-4">
+        {paySuccess}
+      </div>
+      )}
+      {hasError && (
+      <div className="self-center whitespace-pre-wrap break-words mt-4 border border-red-500 rounded p-4">
+        {error}
+      </div>
+      )}
       <div className="panel flex-col">
-        <div className="flex flex-row">
+        <div className="flex flex-row mb-2">
           <div className="inline-flex flex-shrink-0 items-center justify-center h-12 w-12 text-yellow-600 bg-yellow-100 rounded-full mr-6">
             <FaCoins className="text-yellow-500 text-xl" />
           </div>
           <h1>Vrün Balance</h1>
         </div>
-        <div>
-          <span>{0}</span>
-          ETH
-        </div>
+        {creditIsLoading ? (
+          <div className="italic">Loading balance data...</div>
+        ) : creditError ? (
+          <div className="italic">Could not get your current balance</div>
+        ) : (
+          <div>{currentBalance} Days</div>
+        )}
       </div>
-      <div className="panel flex-col">
-        <h2>Add ETH Credit:</h2>
-        <Input type="number" placeholder="Enter ETH amount"></Input>
-        <Button className="btn-primary self-center bg-green-500 data-[hover]:bg-green-700 data-[active]:bg-green-600">
-          Pay
-        </Button>
-      </div>
-    </>
+      <PayForm
+        onSuccess={setPayHash}
+        pricesError={pricesError}
+        prices={prices}
+        pricesIsLoading={pricesIsLoading}
+      />
+      <SignPaymentForm
+        setPayHash={setPayHash}
+        payHash={payHash}
+      />
+    </section>
   );
 };
 
---
+++ src/components/layout/PayForm.tsx
@@ -0,0 +1,94 @@
+import { type FC, type ReactNode, useEffect, useState } from "react";
+import { type BaseError, useSendTransaction, useWaitForTransactionReceipt, useAccount } from 'wagmi';
+import { type TransactionReceipt, parseEther } from 'viem'
+import { FaInfoCircle } from "react-icons/fa";
+import { Button, Input, Popover } from "@headlessui/react";
+import type { UseQueryResult } from "@tanstack/react-query";
+
+export const PayForm: FC<{
+  onSuccess?: (hash: String) => void;
+  pricesError: boolean;
+  pricesIsLoading: boolean;
+  prices?: object;
+}> = ({onSuccess, pricesError, pricesIsLoading, prices}) => {
+  const [value, setValue] = useState("");
+  const [days, setDays] = useState(0);
+  const { data: hash, error: sendTransactionError, isPending, sendTransaction } = useSendTransaction();
+  const { isLoading: isConfirming, isSuccess: isConfirmed } = useWaitForTransactionReceipt({ hash });
+
+  const {address, chainId} = useAccount();
+
+  const submit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    //onSuccess('0xe648c89179122424f26c442530959aae430331530b743c35a91c1caf317f7e58');
+
+    const formData = new FormData(e.target as HTMLFormElement);
+    const to = '0x99E2b1FB1085C392b9091A5505b0Ac27979501F8';
+    sendTransaction({ to, value: parseEther(value) });
+  };
+
+  const inputChanged = (e) => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    if(prices) {
+      setDays(value / prices.pricingRowData.find((contract) => {
+          return contract.tokenAddress === '0x0000000000000000000000000000000000000000' && contract.tokenChainId === chainId;
+        }).price);
+    }
+  }, [value, prices]);
+
+  useEffect(() => {
+    console.log("prices");
+    console.log(prices);
+    if (isConfirmed && onSuccess) onSuccess(hash);
+  }, [isConfirmed, onSuccess, hash, sendTransactionError, prices]);
+
+  useEffect(() => {
+    setValue("");
+  }, [isConfirmed]);
+
+  return (
+    <div className="panel flex-col">
+      <form onSubmit={submit} className="w-full flex flex-col gap-4 justify-center content-center max-w-md">
+        <h2 className="flex">
+          <div className="grow">Add ETH Credit:</div>
+          <div className="relative group">
+            <FaInfoCircle className="text-xl text-yellow-500 cursor-pointer" />
+            <div className="absolute hidden group-hover:block z-10 w-80 px-4 py-2 text-sm text-white bg-gray-800 rounded-lg shadow-md -top-10 left-0">
+              Transfer ETH from your wallet to vrün's Safe wallet. You will be prompted to sign the transaction afterwards as well.
+            </div>
+          </div>
+        </h2>
+        {pricesError && <span>{pricesError.message} </span>}
+        {!pricesError && !pricesIsLoading && (
+        <>
+        <Input
+          value={value}
+          className="border border-slate-200 p-2 rounded-md bg-transparent data-[hover]:shadow"
+          name="amount"
+          type="number"
+          step="0.001"
+          placeholder="Enter ETH amount"
+          onChange={inputChanged}
+          required
+        />
+        <div>{days} days</div>
+        <Button type="submit" className="btn-primary self-center" disabled={value && (isPending || isConfirming || isConfirmed)}>
+          {isPending || isConfirming ? 'Confirming...' : 'Buy Credits'}
+        </Button>
+        </>)}
+        {pricesIsLoading && <div className="italic">Waiting for prices to load...</div>}
+        {pricesError && <div className="italic">Error loading dayprices.</div>}
+        {value && isConfirming && <div className="italic">Waiting for confirmation...</div>}
+        {value && isConfirmed && <div className="italic">Transaction confirmed. Waiting for transaction signing</div>}
+        {sendTransactionError && (
+        <div className="self-center whitespace-pre-wrap break-words mt-4 border border-red-500 rounded p-4">
+          {(sendTransactionError as BaseError).shortMessage || sendTransactionError.message}
+        </div>
+        )}
+      </form>
+    </div>
+  );
+};
