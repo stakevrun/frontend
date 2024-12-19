b57a800cb13e9d5e2ef13ef717118ba94d9ba820
owlot <95369289+owlot@users.noreply.github.com>
2024-12-19 17:20:27 +0100

    feat: add generic transaction signer button

--- src/hooks/useWriteDb.ts
+++ src/hooks/useWriteDb.ts
@@ -7,28 +7,23 @@ export function useWriteDb({
   path,
   method,
   type,
-  data,
 } : {
   path?: number | "batch" | "credit",
   method?: "POST" | "PUT",
   type: string,
-  data: object
 }) {
   const {chainId, address} = useAccount();
   const addPath = typeof path == 'undefined' ? '' : `/${path}`;
   const url = `${API_URL}/${chainId}/${address}${addPath}`;
-  const mutationFn = async ({signature}: {signature: Hex}) => {
+
+  const mutationFn = async ({signature, data}: {signature: Hex, data: object}) => {
     const options = {
       method: method || "POST",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify({type, data, signature})
     };
-    const res = await fetch(url, options);
-    if (!([200, 201].includes(res.status)))
-      throw new Error(`Failed (${res.status}) to ${method} API: ${await res.text()}`);
-    return res;
+    return await fetch(url, options);
   };
-  return useMutation({
-    mutationFn
-  });
+
+  return useMutation({ mutationFn });
 };
