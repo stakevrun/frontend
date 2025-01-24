852442709171f1aebf8798ec73f435639069716f
owlot <95369289+owlot@users.noreply.github.com>
2025-01-24 20:37:20 +0100

    feat: show validators and balance in overview page

--- src/components/account/Overview.tsx
+++ src/components/account/Overview.tsx
@@ -1,32 +1,51 @@
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { FaCoins, FaEthereum, FaMoneyBillWave } from "react-icons/fa";
+import { ValidatorOverview } from "../layout/ValidatorOverview";
+import { CreditOverview } from "../layout/CreditOverview";
 
 const Overview = () => {
+  // prettier-ignore
+  const [hasError, setHasError] = useState<Boolean>(false);
+  const [error,    setError   ] = useState<String>("");
+  const [message,  setMessage ] = useState<String>("");
+
+  const setOverviewError = (error: String) => {
+    setError(error);
+    setHasError(true);
+  };
+
   return (
     <section className="grid gap-4 my-10 py-5">
       <div className="panel flex-col">
         <p>Withdrawal Address:</p>
         <p className="font-bold text-vrun-4 hover:text-vrun-5 dark:hover:text-vrun-2">
-          <Link href={`` /*TODO*/}>Retrieve Signature</Link>
+          <Link href={`` /*TODO*/}>Retrieve Pre-signed Exit Messages</Link>
         </p>
       </div>
-      <div className="panel">
-        <FaEthereum className="text-xl text-blue-500" />
-        <h2 className="mx-4">Validators</h2>
-        <div>{`(list of user's validators and summary of their info goes here)`}</div>
+      <div className="panel flex-col items-start">
+        <div className="flex w-full mb-3 justify-left items-center">
+          <div className="flex items-center h-12 w-12">
+            <FaEthereum className="text-blue-500 text-xl" />
+          </div>
+          <h2 className="flex flex-1 text-lg">Validators</h2>
+        </div>
+        <ValidatorOverview onError={setOverviewError} showCols={["status", "statusTime", "address", "pubkeyShort", "action"]}  />
+      </div>
+      <div className="panel flex-col items-start">
+        <div className="flex w-full items-center">
+          <div className="flex items-center h-12 w-12">
+            <FaCoins className="text-yellow-500 text-xl" />
+          </div>
+          <h2 className="flex flex-1 text-lg">Vrun Balance</h2>
+          <div className="text-lg">
+            <CreditOverview onError={setOverviewError} />
+          </div>
+        </div>
       </div>
       <div className="panel">
-        <FaCoins className="text-xl text-yellow-500" />
-        <h2 className="mx-4">Vrun Balance</h2>
-      </div>
-      <div className="panel">
-        <Image
-          width={20}
-          height={20}
-          alt="Rocket Pool Logo"
-          src={"/images/rocketPlogo.png"}
-        />
+        <Image width={20} height={20} alt="Rocket Pool Logo" src={"/images/rocketPlogo.png"} />
         <h2 className="mx-4">RPL Stake</h2>
       </div>
       <div className="panel">
--- src/pages/validators/index.tsx
+++ src/pages/validators/index.tsx
@@ -1,17 +1,10 @@
-import { type NextPage } from "next";
+import type { NextPage } from "next";
 import { useState } from "react";
 import IfConnected from "../../components/layout/IfConnected";
 import { IfRegistered } from "../../components/layout/IfRegistered";
 import { IfSigned } from "../../components/layout/IfSigned";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "../../components/layout/table";
 import { ValidatorOverview } from "../../components/layout/ValidatorOverview";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../../components/layout/table";
 
 // info about all validators (with links to detail page)
 
@@ -36,9 +29,9 @@ Other info (decide which to include/save for detail pages/exclude):
 
 // fake placeholder
 type Validator = {
-  pubkey: `0x${string}`;
-  index: number;
-  vIndex: number;
+  pubkey: `0x${String}`;
+  index: Number;
+  vIndex: Number;
 };
 
 const setOverviewError = (error: String) => {
@@ -48,15 +41,15 @@ const setOverviewError = (error: String) => {
 
 const Validators: NextPage = () => {
 
-const [hasError, setHasError] = useState(false);
-const [error,    setError]    = useState("");
+const [hasError, setHasError] = useState<Boolean>(false);
+const [error,    setError   ] = useState<String>("");
 
   return (
     <IfConnected>
       <IfRegistered>
         <IfSigned>
           {hasError ? <p>Error getting validators: {error.toString()}</p> : (
-          <ValidatorOverview onError={setOverviewError} />
+          <ValidatorOverview onError={setOverviewError} showCols={["status", "vrunIndex", "index", "address", "pubkey"]} />
           )}
         </IfSigned>
       </IfRegistered>
